43729961ffb906f52543be26eaf121d840b5b8e6
Ivan Varabyou <onebelbiz@gmail.com>
2024-08-25 13:46:37 +0300

    ci: edit environments.ts

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,6 +1,8 @@
 export const environment = {
   production: true,
   apiUrl: 'https://chat-app-backend-9ma9.onrender.com/api',
-  websocketUrl: 'wss://chat-app-backend-9ma9.onrender.com',
   profileImageApi: 'https://api.dicebear.com/8.x/pixel-art/svg?seed=J_',
+  wsConfig: {
+    url: 'wss://chat-app-backend-9ma9.onrender.com',
+  },
 };
