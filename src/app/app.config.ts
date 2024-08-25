afbede41869b20429b23d2e6a0383fff39d4535c
Ivan Varabyou <onebelbiz@gmail.com>
2024-08-25 13:48:44 +0300

    refactor: edit code

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,19 +3,21 @@ import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideStorage } from './core/services';
-import { provideApiToken } from './core/services/api/provideApiTocken';
+import { provideApiToken } from './core/services/api/provide-api-tocken';
 import {
   HTTP_INTERCEPTORS,
   provideHttpClient,
   withFetch,
 } from '@angular/common/http';
 import { AuthInterceptor } from './core/interceptor/auth.interceptor';
+import { provideWebsocket } from './core/services/websocket/provide-websocket';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withHashLocation()),
     provideStorage(window.localStorage),
     provideApiToken(),
+    provideWebsocket(),
     provideHttpClient(withFetch()),
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
   ],
